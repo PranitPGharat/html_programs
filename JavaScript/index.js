6e82e08655ca9e81ea2562eff21c24190ab0a7b8
unknown <pranitgharat1001@gmail.com>
2025-01-18 15:28:16 +0530

    sending html code

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+console.log("welcome to javascript");
+console.log("this is console statement");
+//document.write is not executed in .js
+let a=20, b=10;
+if(a>b){
+    console.log("a is greater")
+}
+else{
+    console.log("b is greater")
+}
